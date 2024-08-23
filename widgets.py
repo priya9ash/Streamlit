44d2e28f69dfa7c9613de67e1f68e11834137ac3
deb-ashwin <165624038+deb-ashwin@users.noreply.github.com>
2024-08-24 01:09:17 +0530

    save the changes

--- widgets.py
+++ widgets.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 st.title("Streamit Text input")
 
 name= st.text_input("Enter your name")
@@ -11,4 +12,20 @@ if name:
 
 age= st.slider('Select your age ', 0,100,25) 
 
-st.write("Your age is {age}")
+st.write(f"Your age is {age}")
+data= {
+"Name":['John','Jane','Jack','Jill'],"Age": [23,45,67,34],'City':['New York','Los Angels','Chicago','Houston']
+
+
+}
+df=pd.DataFrame(data)
+df.to_csv('sampledata.csv')
+st.write(df)
+
+
+
+
+uploaded_file= st.file_uploader('Choose a CSV file',type ='csv')
+if uploaded_file is not None:
+    df= pd.read_csv(uploaded_file)
+    st.write(df)
